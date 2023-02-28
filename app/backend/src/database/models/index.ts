e00d3899c3d7d136b50005c4d32370f336497bd0
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-02-28 14:52:36 -0300

    Teste

--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -4,3 +4,7 @@ import * as config from '../config/database';
 const sequelize = new Sequelize(config)
 
 export default sequelize;
+
+
+export { default as TeamModel } from './UsersModel';
+export  { default as UserModel } from './UsersModel';
